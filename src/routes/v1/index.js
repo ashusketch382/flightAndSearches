4fb47133175f86bf04ee2bc99cc38b33d9271f16
Ashutosh Singh <ashutoshsi@prod.hclpnp.com>
2022-12-25 18:51:04 +0530

    added more routes to complete api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -4,5 +4,8 @@ const cityController = require("../../controllers/city-controller");
 const router = express.Router();
 
 router.post('/city', cityController.create);
+router.get('/city/:id', cityController.get);
+router.patch('/city/:id', cityController.update);
+router.delete('/city/:id', cityController.destroy);
 
 module.exports = router;
